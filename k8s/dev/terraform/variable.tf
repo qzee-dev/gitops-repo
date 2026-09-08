6a0df53d98e57c4cf9e26eb11ffc4137568f7746
Qoseem Oluwaphemy <tech.Assistancebas@gmail.com>
2026-09-08 15:12:45 +0100

    Add Terraform variables for AWS and namespaces

--- k8s/dev/terraform/variable.tf
+++ k8s/dev/terraform/variable.tf
@@ -1,1 +1,18 @@
+variable "aws_region" {
+  type    = string
+  default = "eu-west-1"
+}
 
+variable "cluster_name" {
+  type    = string
+  default = "my-eks-cluster"
+}
+
+variable "argocd_namespace" {
+  type    = string
+  default = "argocd"
+}
+
+variable "app_namespace" {
+  type    = string
+  default = "myapp"
