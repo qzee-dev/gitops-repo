1e31b04396522d540df4b5421a03698873598d1c
Qoseem Oluwaphemy <tech.Assistancebas@gmail.com>
2026-09-08 15:51:49 +0100

    Define IAM role and policy for load balancer
    Added IAM role and policy for AWS Load Balancer Controller.

--- k8s/dev/terraform/aws-load-balancer-controller.tf
+++ k8s/dev/terraform/aws-load-balancer-controller.tf
@@ -1,1 +1,47 @@
+######################################
+#IAM Policy
+######################################
+resource "aws_iam_role" "aws_load_balancer_controller" {
+  name = "${var.cluster_name}-aws-load-balancer-controller"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+
+    Statement = [
+      {
+        Effect = "Allow"
+
+        Principal = {
+          Service = "pods.eks.amazonaws.com"
+        }
+
+        Action = [
+          "sts:AssumeRole",
+          "sts:TagSession"
+        }
+      }
+    ]
+  })
+}
+
+######################################
+#IAM Role
+#####################################
+
+
+
+
+
+#####################################
+# Pod Identity Association
+#####################################
+
+
+
+
+#####################################
+#Helm Release
+#####################################
+
+
 
