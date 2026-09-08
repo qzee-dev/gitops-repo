075ecb77ee928b4523c66d77d3d54fc11198a6ca
Qoseem Oluwaphemy <tech.Assistancebas@gmail.com>
2026-09-08 15:36:36 +0100

    Implement EKS Pod Identity addon and association
    Added EKS Pod Identity addon and association resource.

--- k8s/dev/terraform/eks-pod-identity.tf
+++ k8s/dev/terraform/eks-pod-identity.tf
@@ -1,4 +1,6 @@
-
+#########################################################
+#Eks Pod Identity Addon
+########################################################
 resource "aws_eks_addon" "pod_identity_agent" {
   cluster_name = var.cluster_name
   addon_name   = "eks-pod-identity-agent"
@@ -11,4 +13,14 @@ resource "aws_eks_addon" "pod_identity_agent" {
     ManagedBy   = "Terraform"
     Addon       = "eks-pod-identity-agent"
   }
+}
+
+#########################################################
+#Pod identity assosciation
+###########################################################
+resource "aws_eks_pod_identity_association" "myapp" {
+  cluster_name    = var.cluster_name
+  namespace       = var.app_namespace
+  service_account = "myapp"
+  role_arn        = aws_iam_role.myapp.arn
 }
