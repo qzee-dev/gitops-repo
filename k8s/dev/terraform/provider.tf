eb122a0b3d76d8c070a4fa0fa47b746724d0966d
Qoseem Oluwaphemy <tech.Assistancebas@gmail.com>
2026-09-08 15:56:05 +0100

    Configure Helm provider in Terraform
    Added Helm provider configuration for EKS integration.

--- k8s/dev/terraform/provider.tf
+++ k8s/dev/terraform/provider.tf
@@ -11,3 +11,46 @@ terraform {
 provider "aws" {
   region = var.aws_region
 }
+
+#########################################
+#Helm provider
+########################################
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+    }
+
+    helm = {
+      source = "hashicorp/helm"
+    }
+  }
+}
+
+provider "aws" {
+  region = var.aws_region
+}
+
+provider "helm" {
+  kubernetes {
+    host                   = data.aws_eks_cluster.this.endpoint
+    cluster_ca_certificate = base64decode(
+      data.aws_eks_cluster.this.certificate_authority[0].data
+    )
+
+    exec {
+      api_version = "client.authentication.k8s.io/v1beta1"
+      command     = "aws"
+
+      args = [
+        "eks",
+        "get-token",
+        "--cluster-name",
+        var.cluster_name,
+        "--region",
+        var.aws_region
+      ]
+    }
+  }
+}
+
