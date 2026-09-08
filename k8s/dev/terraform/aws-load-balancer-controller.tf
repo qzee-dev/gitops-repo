4a9d2ce8504e59276757254d25fd93c53bfd601e
Qoseem Oluwaphemy <tech.Assistancebas@gmail.com>
2026-09-08 15:59:00 +0100

    Update aws-load-balancer-controller.tf

--- k8s/dev/terraform/aws-load-balancer-controller.tf
+++ k8s/dev/terraform/aws-load-balancer-controller.tf
@@ -35,13 +35,48 @@ resource "aws_iam_role" "aws_load_balancer_controller" {
 #####################################
 # Pod Identity Association
 #####################################
+resource "aws_eks_pod_identity_association" "aws_load_balancer_controller" {
+  cluster_name    = var.cluster_name
+  namespace       = "kube-system"
+  service_account = "aws-load-balancer-controller"
 
-
-
-
+  role_arn = aws_iam_role.aws_load_balancer_controller.arn
+}
 #####################################
 #Helm Release
 #####################################
+resource "helm_release" "aws_load_balancer_controller" {
+  name       = "aws-load-balancer-controller"
+  repository = "https://aws.github.io/eks-charts"
+  chart      = "aws-load-balancer-controller"
+  namespace  = "kube-system"
+
+  set {
+    name  = "clusterName"
+    value = var.cluster_name
+  }
+
+  set {
+    name  = "region"
+    value = var.aws_region
+  }
+
+  set {
+    name  = "vpcId"
+    value = var.vpc_id
+  }
+
+  set {
+    name  = "serviceAccount.create"
+    value = "true"
+  }
+
+  set {
+    name  = "serviceAccount.name"
+    value = "aws-load-balancer-controller"
+  }
+}
+
 
 
 
