92d28831408b04c5f86bcc2ed4a773388a39cb3a
Qoseem Oluwaphemy <tech.Assistancebas@gmail.com>
2026-09-08 15:24:13 +0100

    Update eks-pod-identity.tf

--- k8s/dev/terraform/eks-pod-identity.tf
+++ k8s/dev/terraform/eks-pod-identity.tf
@@ -1,1 +1,9 @@
 
+resource "aws_eks_addon" "pod_identity_agent" {
+  cluster_name = var.cluster_name
+  addon_name   = "eks-pod-identity-agent"
+
+  tags = {
+    Name = "${var.cluster_name}-pod-identity-agent"
+  }
+}
