235ebec0cd5100f3e04806132da8d9e4d8838e4c
Qoseem Oluwaphemy <tech.Assistancebas@gmail.com>
2026-09-08 15:12:13 +0100

    Update provider.tf

--- k8s/dev/terraform/provider.tf
+++ k8s/dev/terraform/provider.tf
@@ -1,1 +1,13 @@
+terraform {
+  required_version = ">= 1.5.0"
 
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+    }
+  }
+}
+
+provider "aws" {
+  region = var.aws_region
+}
