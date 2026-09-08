5c1a9e7d3612ee6776157820d5353c4c0f500414
Qoseem Oluwaphemy <tech.Assistancebas@gmail.com>
2026-09-08 19:14:09 +0100

    Define outputs for ECR repository details
    Added outputs for ECR repository ARNs and registry URL.

--- Ecr-Repo/output.tf
+++ Ecr-Repo/output.tf
@@ -6,3 +6,20 @@ output "ecr_repositories" {
     service => repository.repository_url
   }
 }
+
+
+
+output "ecr_repository_arns" {
+  description = "ECR repository ARNs indexed by microservice"
+
+  value = {
+    for service, repository in aws_ecr_repository.microservice :
+    service => repository.arn
+  }
+}
+
+output "ecr_registry_url" {
+  description = "AWS ECR registry URL"
+
+  value = split("/", values(aws_ecr_repository.microservice)[0].repository_url)[0]
+}
